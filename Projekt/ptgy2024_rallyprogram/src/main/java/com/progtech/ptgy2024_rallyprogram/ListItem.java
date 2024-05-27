d00b57438625b2ddb20444911ad2668edddccaa5
Doomer022 <83778424+Doomer022@users.noreply.github.com>
2024-05-27 19:19:25 +0200

    Listázási alapok

---
+++ Projekt/ptgy2024_rallyprogram/src/main/java/com/progtech/ptgy2024_rallyprogram/ListItem.java
@@ -0,0 +1,10 @@
+package com.progtech.ptgy2024_rallyprogram;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ListItem {
+
+    public ImageView item_icon;
+    public Label item_label;
+}
--- Projekt/ptgy2024_rallyprogram/src/main/java/com/progtech/ptgy2024_rallyprogram/ProgramController.java
+++ Projekt/ptgy2024_rallyprogram/src/main/java/com/progtech/ptgy2024_rallyprogram/ProgramController.java
@@ -1,16 +1,29 @@
 package com.progtech.ptgy2024_rallyprogram;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class ProgramController {
 
+    /*FXML SPECIFIC*/
     public ImageView countryButton;
     public ImageView stageButton;
     public ImageView driverButton;
     public ImageView carButton;
+
+    public VBox itemListBox;
+
+    @FXML
+    public void initialize()
+    {
+        TestScrollPane(10);
+    }
 
     @FXML
     public void onCountryButtonMouseEnter(MouseEvent e) { ButtonHover(countryButton, true); }
@@ -36,11 +49,28 @@ public class ProgramController {
     @FXML
     public void onCarButtonMouseExit(MouseEvent e) { ButtonHover(carButton, false); }
 
+
     void ButtonHover(ImageView imgView, boolean hover) {
         if (hover) {
-            imgView.setStyle("-fx-effect: innershadow(gaussian, green, 7, 1, 1, 1)");
+            imgView.setStyle("-fx-effect: innershadow(gaussian, green, 20, 1, 1, 1)");
         } else {
             imgView.setStyle("");
+        }
+    }
+
+    void TestScrollPane(int listSize)
+    {
+        for(int i = 0; i < listSize; i++)
+        {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("list-item.fxml"));
+                Parent root = (Parent)loader.load();
+                ListItem itemControl = loader.getController();
+                itemControl.item_label.setText("Rally Program Teszt");
+                itemControl.item_icon.setImage(new Image(getClass().getResourceAsStream("img/hungary.png")));
+                itemListBox.getChildren().add(root);
+            }
+            catch (Exception ex) { ex.printStackTrace(); }
         }
     }
 }
